8150679fd5b96eb4b35709c306576e537187e123
@maden <166928262+maden77@users.noreply.github.com>
2025-06-20 13:33:28 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,13 +2,13 @@ self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("ulartangga-v1").then(cache => {
       return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./manifest.json",
-        "./icons/icon-192.png",
-        "./icons/icon-512.png"
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js",
+        "/manifest.json",
+        "/icons/icon-192.png",
+        "/icons/icon-512.png"
       ]);
     })
   );
